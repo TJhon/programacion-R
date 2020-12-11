a258aae29d38bec9f2223ab8cd4345642f13a3c2
TJhon <fr.jhonk@gmail.com>
2020-12-11 11:05:14 -0500

    primeros pasos

---
+++ 02Inicio_programacion/01basic.r
@@ -0,0 +1,80 @@
+# basico en R
+# Operaciones matematicas
+1 + 2
+1 - 2
+3 * 9
+4 / 37
+2 ^ 3
+
+## asiganciones de variables
+# usas las variables
+a <- 11
+
+b <- 98
+
+a + b
+a - b
+a * b
+a / b
+b ^ a
+b * 2
+
+## numeros 
+
+c <- 2.3
+c * 12
+
+## comparaciones SI NO ingles
+
+1 < 2
+2 > 3
+
+2 <= 3
+2 <= 1
+
+2 >= 12
+12 >= 2
+
+# vairables 
+a < b
+b >= a
+
+# 1 0 
+
+verdadero <- a < b
+verdadero
+
+
+## igual a ,  diferente
+
+2 == 2
+2 == 3
+2 != 3
+
+var1 <- 1 == 3
+var2 <- 1 != 3
+
+# comparar string letras variables no numericas
+
+a == "a"
+98 == "b"
+
+
+"a" == "A"
+
+
+# personas que tengan la letra "A" como primer lentra de su nombre
+
+"a" != "A"
+
+# comparacion de vairables
+
+a <- 11
+A <- 13
+b <- 98
+
+a == A
+# genera un error de no encontrar el archivo
+B
+
+
